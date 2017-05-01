6aa9b79e7a26f08cb3f50dfadc8a8a6bff865d80
mika <mika@mail.com>
2017-05-01 21:16:10 +0300

    asd

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Mika\Documents\Visual Studio 2017\Projects\mysite\mysite\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
